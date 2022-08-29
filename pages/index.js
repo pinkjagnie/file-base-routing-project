da04e8b7e6390bb7663b136adb3390cea846940a
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-08-29 19:25:28 +0200

    Added EventItem & EventList with styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import EventList from "../components/events/event-list";
+
+import { getFeaturedEvents } from "../dummy-data";
+
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>The home page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
