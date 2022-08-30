28140a3cec9b859d60270af672c484b91acf721e
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-08-30 18:34:04 +0200

    Add _document.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Layout from "../components/layout/layout";
 
 import "../styles/globals.css";
@@ -5,6 +7,11 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>NextJS events</title>
+        <meta name="description" content="NextJS events in your town" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
