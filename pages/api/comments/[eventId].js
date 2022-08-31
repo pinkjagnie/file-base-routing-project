b6c3c37b559b0fefecd61a877ac948e681bd16f7
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-08-31 17:48:08 +0200

    Added error handling

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,20 @@
-function handler(req, res) {
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from "../../../helpers/db-util";
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  let client;
+
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({ message: "Connecting to the database failed" });
+    return;
+  }
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -12,32 +27,41 @@ function handler(req, res) {
       text.trim() === ""
     ) {
       res.status(422).json({ message: "Invalid input" });
+      client.close();
       return;
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
 
-    console.log(newComment);
+    let result;
 
-    res
-      .status(201)
-      .json({ message: "Success. Comment added", comment: newComment });
+    try {
+      result = await insertDocument(client, "comments", newComment);
+      newComment._id = result.insertedId;
+
+      res
+        .status(201)
+        .json({ message: "Success. Comment added", comment: newComment });
+    } catch (error) {
+      res.status(500).json({ message: "Inserting comments failed" });
+    }
   }
 
   if (req.method === "GET") {
-    const dummyCommentList = [
-      { id: "c1", name: "Max", text: "A first comment" },
-      { id: "c2", name: "Manu", text: "A second comment" },
-      { id: "c3", name: "Abc", text: "A third comment" },
-    ];
-
-    res.status(200).json({ comments: dummyCommentList });
+    try {
+      const documents = await getAllDocuments(client, "comments", { _id: -1 });
+      res.status(200).json({ comments: documents });
+    } catch (error) {
+      res.status(500).json({ message: "Getting comments failed" });
+    }
   }
+
+  client.close();
 }
 
 export default handler;
